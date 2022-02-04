4de26e9307a889b9d911fdb9326ee1da908cd43d
MadBear422 <supermarioland100@gmail.com>
2022-02-04 13:00:42 -0800

    you should fix this code NOW

--- source/editors/BackgroundEditorState.hx
+++ source/editors/BackgroundEditorState.hx
@@ -9,11 +9,29 @@ import flixel.ui.FlxSpriteButton;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxObject;
+
+#if MODS_ALLOWED
 import sys.FileSystem;
+#end
 
 class BackgroundEditorState extends MusicBeatState
 {
+    public var BF_X:Float = 770;
+	public var BF_Y:Float = 100;
+	public var DAD_X:Float = 100;
+	public var DAD_Y:Float = 100;
+	public var GF_X:Float = 400;
+	public var GF_Y:Float = 130;
+
     override function create() {
         
     }
+    function startCharacterPos(char:Character, ?gfCheck:Bool = false) {
+		if(gfCheck && char.curCharacter.startsWith('gf')) { //IF DAD IS GIRLFRIEND, HE GOES TO HER POSITION
+			char.setPosition(GF_X, GF_Y);
+			char.scrollFactor.set(0.95, 0.95);
+		}
+		char.x += char.positionArray[0];
+		char.y += char.positionArray[1];
+	}
 }
