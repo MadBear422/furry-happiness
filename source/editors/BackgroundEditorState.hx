109b5113ba9caffb2b87442cbf52dd4855f4cccd
CanadianGooseOnGitHub <blake.a.macarthur@gmail.com>
2022-02-04 14:32:47 -0500

    you should commit your code NOW

---
+++ source/editors/BackgroundEditorState.hx
@@ -0,0 +1,19 @@
+package editors;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import haxe.Json;
+import haxe.format.JsonParser;
+import flixel.ui.FlxSpriteButton;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import sys.FileSystem;
+
+class BackgroundEditorState extends MusicBeatState
+{
+    override function create() {
+        
+    }
+}
